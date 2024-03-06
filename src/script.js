b01186b06166ef8a6858f5f602b1d2beb9004467
Flávia Reis <flaviarster@gmail.com>
2024-03-06 06:38:57 -0300

    Finished the script

--- src/script.js
+++ src/script.js
@@ -5,8 +5,15 @@ const error = document.querySelector('#error')
 
 function formValidation(event) {
     event.preventDefault()
+
+    if (email.value.trim() === '') {
+        email.classList.add('invalid')
+        email.setCustomValidity('Please provide an email')
+        error.innerText = 'Please provide an email'
+        error.innerText = email.validationMessage
+    } 
     
-    if (!email.checkValidity()) {
+    else if (!email.checkValidity()) {
         email.classList.add('invalid')
         email.setCustomValidity('Please provide a valid email')
         email.value = 'janeappleseed@email.com'
